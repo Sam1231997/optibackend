eaf50488ff5619b7b8c8540525d5baddfe3c5849
Sam1231997 <161332692+Sam1231997@users.noreply.github.com>
2025-01-25 08:59:40 -1200

    Update app.js

--- app.js
+++ app.js
@@ -111,4 +111,4 @@ app.put('/submissions/:id', async (req, res) => {
 
 // Start the server
 
-app.listen(port, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
